5b55768e1ac2a356f385153d37ffd70958acac3f
Muhammad Khairul Faisal bin Muhamad Atan <73818703+khairulfaisal97@users.noreply.github.com>
2020-12-22 21:14:02 +0800

    Create EXP_2.ino

---
+++ Week 6/EXP_2/EXP_2.ino
@@ -0,0 +1,16 @@
+void setup() {
+  //  to run once:
+  unsigned char* ddrd = (unsigned char*) 0x2A;
+  unsigned char* ocr0b = (unsigned char*) 0x48;
+  unsigned char* tccr0a = (unsigned char*) 0x44;
+  unsigned char* tccr0b = (unsigned char*) 0x45;
+  *ddrd = 1 << 5;
+  *ocr0b = 127;
+  *tccr0a = 0b00100011;
+  *tccr0b = 0b00000001;
+}
+
+void loop() {
+  // to run repeatedly:
+
+}
